adc42c57f9e13109996699f015fe5c6af6f46277
Christopher Lozzi <clozzi12@gmail.com>
2023-08-15 13:41:41 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,31 +1,11 @@
 function fetchBooks() {
-  fetch("https://anapioficeandfire.com/api/books")
-  .then((resp) => resp.json())
-  .then((books) => renderBooks(books));  
+  return fetch ("https://anapioficeandfire.com/api/books")
+    .then (resp => resp.json())
+    .then (data => renderBooks(data));
 }
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
-
-document.addEventListener('DOMContentLoaded', fetchBooks, fetchDogs);
-
-function fetchDogs() {
-  fetch("https://dog.ceo/api/breeds/image/random")
-  .then((resp) => resp.json())
-  .then((image) => renderBooks(image));  
-}
-
-function renderImages(images) {
-  const main = document.querySelector('main');
-  images.forEach(pic => {
-    const img = document.createElement('img');
-    img.innerHTML = pic.random;
-    main.appendChild(img);
-  })
+function renderBooks(data) {
+  for (const key in data) {
+    return (data[key]["name"]);
+  }
 }
